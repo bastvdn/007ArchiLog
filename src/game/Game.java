b15ddac3e23fdd7fd941183a3355adc033ef3d28
Horsefactor <36157836+Horsefactor@users.noreply.github.com>
2021-01-07 03:32:49 +0100

    add test class

--- src/game/Game.java
+++ src/game/Game.java
@@ -28,7 +28,7 @@ public class Game {
         }
     }
 
-    private Game() throws InterruptedException {
+    protected Game() throws InterruptedException {
         System.out.println("WELCOME IN 007 RPG");
         TimeUnit.SECONDS.sleep(1);
         System.out.println("???WHAT IS YOUR NAME HOMIE???");
@@ -171,22 +171,22 @@ public class Game {
             System.out.println("Health points: when it goes to zero you die");
             System.out.println("Attack: the damage you deal to the enemy when you attack");
             System.out.println("bullets: the number of attack charges that you got");
-            System.out.println("");
+            System.out.println();
             System.out.println("each turn you can choose between: ");
             System.out.println("Attacking that will cost you bullets");
             System.out.println("Protect, that will block incoming enemy damages");
             System.out.println("And Reloading that will restore one bullet");
-            System.out.println("");
+            System.out.println();
             System.out.println("You and the mobs also got an armor and a weapon");
             System.out.println("The armor allows you to block some damages and regenerates between fights");
             System.out.println("your current armor is " + player.getArmor().getClass().getSimpleName() + " which is not very usefull" );
-            System.out.println("");
+            System.out.println();
             System.out.println("The weapon gives you additional damages, self-healing and critical chances which will allow you to hit twice!!");
 
             System.out.println("Beware : some weapon will cost more bullets per attacks, keep that in mind");
             System.out.println("You also won't be able to see your bullets in the middle of a fight, so keep track of your bullet count!! ");
             System.out.println("your current weapon is " + player.getWeapon().getClass().getSimpleName() + " which is pretty bad" );
-            System.out.println("");
+            System.out.println();
             System.out.println("but don't worry you can drop weapons and armor on mobs!" );
             loading();
             System.out.println("Press enter when you are ready" );
---
+++ src/game/test/GameTest.java
@@ -0,0 +1,27 @@
+package game.test;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest extends game.Game {
+
+    protected GameTest() throws InterruptedException {
+        super();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getInstance() {
+    }
+
+    @Test
+    void loop() {
+    }
+
+    @Test
+    void testGetInstance() {
+
+    }
+
+    @Test
+    void testLoop() {
+    }
+}
